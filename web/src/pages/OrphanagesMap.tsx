78f709cda08833d6988c75211a92f324f811fb5e
Anna Laura <annalaura@pop-os.localdomain>
2020-10-12 18:31:43 -0300

    feat: add Open Street Map to OrphanagesMap page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {FiPlus} from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
-import '../styles/pages/orphanages-map.css'
+import '../styles/pages/orphanages-map.css';
+
 
 function OrphanagesMap(){
   return(
@@ -19,7 +23,13 @@ function OrphanagesMap(){
           <span>São Paulo</span>
         </footer>
       </aside>
-      <div></div>
+      <Map
+        center={[-20.1607,-47.4247]}
+        zoom={15}
+        style={{width: "100%", height: "100%"}}
+      > 
+      <TileLayer  url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#fff"/>
       </Link>
