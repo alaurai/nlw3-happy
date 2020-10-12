bdcf153e67a86326ecce2bd7548b293f137166e8
Anna Laura <annalaura@pop-os.localdomain>
2020-10-12 16:15:17 -0300

    feat: add routes file

---
+++ web/src/routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap'
+
+function routes(){
+  return(
+    <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Landing}/>
+      <Route path="/app" component={OrphanagesMap}/>
+    </Switch>
+
+    </BrowserRouter>
+  );
+}
+
+export default routes;
