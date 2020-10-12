db03cb6d152a973a0bdb6df9dd4100c152b2f526
Anna Laura <annalaura@pop-os.localdomain>
2020-10-12 15:53:47 -0300

    feat: add page-landing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import {FiArrowRight} from 'react-icons/fi'
+
+import './styles/global.css'
+import './styles/pages/landing.css'
+import logoImg from './images/logo.svg'
 
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello Happy!</h1>
+    <div id="page-landing">
+     <div className="content-wrapper">
+      <img src={logoImg} alt="Logo do Happy"/>
+      <main>
+        <h1>Leve felicidade para o mundo</h1>
+        <p>Visite orfanatos e mude o dia
+        de muitas crianças.</p>
+      </main>
+      <div className="location">
+        <strong>Pedregulho</strong>
+        <span>São Paulo</span>
+      </div>
+      <a href="#" className="enter-app">
+        <FiArrowRight size={26} color="rgba(0,0,0,0.6)"/>
+      </a>
+     </div>
     </div>
   );
 }
