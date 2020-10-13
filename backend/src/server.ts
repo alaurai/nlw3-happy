6676a43467641a1887c8814f350c6714ba4d48c7
Anna Laura <annalaura@pop-os.localdomain>
2020-10-13 09:42:11 -0300

    feat: add database connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
-import express, { response } from 'express';
+import express from 'express';
+
+import './database/connection';
 
 const app = express();
 
-app.get("/", (request, response) =>{
+app.get("/users", (request, response) =>{
   return response.json({message: "Hello Happy!" })
 })
 app.listen(3333);
