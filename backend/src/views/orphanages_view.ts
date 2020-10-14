e057f5e3d29d6d15b961ea558420b03869ddf0c2
Anna Laura <annalaura@pop-os.localdomain>
2020-10-14 11:29:01 -0300

    feat: add orphanages view

---
+++ backend/src/views/orphanages_view.ts
@@ -0,0 +1,21 @@
+
+import Orphanage from '../models/Orphanage';
+
+export default {
+  render(orphanage: Orphanage){
+    return{
+      id: orphanage.id,
+      name: orphanage.name,
+      latitude: orphanage.latitude,
+      longitude: orphanage.longitude,
+      about: orphanage.about,
+      instructions: orphanage.instructions,
+      opening_hours: orphanage.opening_hours,
+      open_on_weekends: orphanage.open_on_weekends
+    };
+  },
+
+  renderMany(orphanages: Orphanage[]){
+    return orphanages.map(orphanage => this.render(orphanage));
+  }
+}
