472ec8ac953da88567b6c732f48bba37e3238053
Anna Laura <annalaura@pop-os.localdomain>
2020-10-14 11:37:29 -0300

    feat: add images view

--- backend/src/views/orphanages_view.ts
+++ backend/src/views/orphanages_view.ts
@@ -1,5 +1,6 @@
 
 import Orphanage from '../models/Orphanage';
+import imagesView from './images_view';
 
 export default {
   render(orphanage: Orphanage){
@@ -11,7 +12,8 @@ export default {
       about: orphanage.about,
       instructions: orphanage.instructions,
       opening_hours: orphanage.opening_hours,
-      open_on_weekends: orphanage.open_on_weekends
+      open_on_weekends: orphanage.open_on_weekends,
+      images: imagesView.renderMany(orphanage.images)
     };
   },
 
