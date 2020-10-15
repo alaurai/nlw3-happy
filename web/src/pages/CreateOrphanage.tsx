99e2cf70b223235bcf0e0b06bd82817d77b89900
Anna Laura <annalaura@pop-os.localdomain>
2020-10-15 09:02:38 -0300

    feat: add Sidebar component and its styles

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -3,11 +3,12 @@ import { Map, Marker, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
 import { useHistory } from "react-router-dom";
 
-import { FiArrowLeft, FiPlus } from "react-icons/fi";
+import Sidebar from '../components/Sidebar';
 
 import mapMarkerImg from '../images/map-marker.svg';
-
 import '../styles/pages/create-orphanage.css';
+import { FiPlus } from 'react-icons/fi';
+
 
 const happyMapIcon = L.icon({
   iconUrl: mapMarkerImg,
@@ -18,19 +19,11 @@ const happyMapIcon = L.icon({
 })
 
 export default function CreateOrphanage() {
-  const { goBack } = useHistory();
-
+ 
   return (
     <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
 
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar/>
 
       <main>
         <form className="create-orphanage-form">
--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
-import { FiClock, FiInfo, FiArrowLeft } from "react-icons/fi";
+import { FiClock, FiInfo } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
-import { useHistory } from 'react-router-dom';
 import L from 'leaflet';
+
+import Sidebar from '../components/Sidebar';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -11,26 +12,16 @@ import '../styles/pages/orphanage.css';
 
 const happyMapIcon = L.icon({
   iconUrl: mapMarkerImg,
-
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [0, -60]
 })
 
 export default function Orphanage() {
-  const { goBack } = useHistory();
-
   return (
     <div id="page-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
 
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar/>
 
       <main>
         <div className="orphanage-details">
