5f027f37651f7943facdf25fe3792c4747f978d7
Anna Laura <alauraivani@gmail.com>
2020-10-16 12:19:44 -0300

    feat: add quantity of orphanages found and create orphanage button

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView ,{Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
 
 import mapMarker from './src/images/map-marker.png';
 
@@ -36,6 +37,12 @@ export default function App() {
           </Callout>
         </Marker>
       </MapView>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={()=>{}}>
+          <Feather name="plus" size={20} color="#fff"/>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -60,5 +67,30 @@ const styles = StyleSheet.create({
   calloutText:{
     color: "#0089a5",
     fontSize: 14
+  },
+  footer:{
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    elevation: 3
+  },
+  footerText:{
+    color: '#8fa7b3'
+  },
+  createOrphanageButton:{
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
