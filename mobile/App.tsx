abc7fb818f8d8e51c6f203b2c1a8f7ce111fd54a
Anna Laura <alauraivani@gmail.com>
2020-10-16 13:21:15 -0300

    feat: add and set routes file

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,6 +4,8 @@ import { StyleSheet } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
+import Routes from './src/routes';
+
 export default function App() {
 
   const [fontsLoaded] = useFonts({
@@ -17,7 +19,7 @@ export default function App() {
   }
 
   return (
-    <h1>Hi!</h1>
+    <Routes />
   );
 }
 
