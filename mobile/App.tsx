4b31c98c1928fecff1c22e4765aa8d127f1c6656
Anna Laura <alauraivani@gmail.com>
2020-10-16 12:32:04 -0300

    feat: set useFonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,10 +3,23 @@ import React from 'react';
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView ,{Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
 import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold, 
+    Nunito_700Bold, 
+    Nunito_800ExtraBold
+  });
+
+  if(!fontsLoaded){
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -65,6 +78,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   calloutText:{
+    fontFamily: 'Nunito_700Bold',
     color: "#0089a5",
     fontSize: 14
   },
@@ -83,6 +97,7 @@ const styles = StyleSheet.create({
     elevation: 3
   },
   footerText:{
+    fontFamily: 'Nunito_700Bold',
     color: '#8fa7b3'
   },
   createOrphanageButton:{
