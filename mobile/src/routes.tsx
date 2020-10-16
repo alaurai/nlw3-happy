a5f5070b54b6026d6dccf89477dad99b0a5fdb8e
Anna Laura <alauraivani@gmail.com>
2020-10-16 18:50:07 -0300

    feat: add OrphanageDetails page and set its navigation in the routes

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -5,12 +5,25 @@ import { createStackNavigator } from '@react-navigation/stack';
 const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+import Header from './components/Header';
 
 export default function Routes(){
   return(
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false}}>
-        <Screen name="OrphanagesMap" component={OrphanagesMap}/>
+      <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5'}}}>
+        <Screen 
+          name="OrphanagesMap" 
+          component={OrphanagesMap}
+        />
+        <Screen 
+          name="OrphanageDetails" 
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Orfanato" showCancel={false}/>
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   )
