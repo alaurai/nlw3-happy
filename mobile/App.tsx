7bbe480c9b158de2d3085a2ad4e9db2331d59141
Anna Laura <alauraivani@gmail.com>
2020-10-16 09:48:23 -0300

    feat: add map marker

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView ,{PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView ,{Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
@@ -14,7 +16,26 @@ export default function App() {
           longitude: -47.4766,
           latitudeDelta: 0.008,
           longitudeDelta: 0.008
-        }}/>
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: -20.25694,
+            longitude: -47.4766,
+          }}
+        >
+          <Callout tooltip={true} onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das Meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -27,5 +48,17 @@ const styles = StyleSheet.create({
   map:{
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+  calloutContainer:{
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255,255,255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+  calloutText:{
+    color: "#0089a5",
+    fontSize: 14
   }
 });
