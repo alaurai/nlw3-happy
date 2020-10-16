cc928d0fd192c43b08789f6461141ad69951532a
Anna Laura <alauraivani@gmail.com>
2020-10-16 09:22:11 -0300

    feat: add and config MapView

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView ,{PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Happy Mobile!</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -20.25694,
+          longitude: -47.4766,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +23,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map:{
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
